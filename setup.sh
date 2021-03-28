96e77efa7f67f393aa0ca6c8c28ea506bd2954f7
Navin <navinds25@gmail.com>
2021-03-29 05:19:24 +0530

    changed to allow for podman

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/navinds25/DotFiles.git
 echo "DOTHOME=\"${PWD}/DotFiles\"" >> ~/.bashrc
-echo 'source "$DOTHOME"/Containers/bash_dockerfunc' >> ~/.bashrc
+echo 'source "$DOTHOME"/Containers/bash_containerfunc' >> ~/.bashrc
 echo 'source "$DOTHOME"/Containers/bash_kubernetesfunc' >> ~/.bashrc
 echo 'source "$DOTHOME"/System/bash_systemfunc' >> ~/.bashrc
