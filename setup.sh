ac2e1655dedb19ae2fcffd91066d05004389e4cb
Navin Sadanandan <navinds25@gmail.com>
2021-08-07 14:01:02 +0530

    changed git from http to ssh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/navinds25/DotFiles.git
+git clone git@github.com:navinds25/DotFiles.git
 echo "DOTHOME=\"${PWD}/DotFiles\"" >> ~/.bashrc
 echo 'source "$DOTHOME"/Containers/bash_containerfunc' >> ~/.bashrc
 echo 'source "$DOTHOME"/Containers/bash_kubernetesfunc' >> ~/.bashrc
